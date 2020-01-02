1c981dfbfeaa1081272c73ca08e932c110cea2a0
SethKalkstein <sethkal7@yahoo.com>
2020-01-02 10:40:55 -0500

    previous challelge with loops tuples and for of

--- challenge1.js
+++ challenge1.js
@@ -2,3 +2,24 @@
 var isDivisible = function (n, x, y) { return n % x === 0 && n % y == 0 ? true : false; };
 //count the characters that are duplicated
 var duplicateCount = function (text) { return text.toLowerCase().split("").sort().reduce(function (acc, cur, ind, arr) { return acc += (cur == arr[ind + 1] && cur != arr[ind - 1]) ? 1 : 0; }, 0); };
+var duplicateCount2 = function (text) {
+    var tupCounter = [];
+    var count = 0;
+    for (var _i = 0, _a = text.toLowerCase(); _i < _a.length; _i++) {
+        var textChar = _a[_i];
+        var newLetter = true;
+        for (var j = 0; j < tupCounter.length; j++) {
+            if (textChar === tupCounter[j][0]) {
+                tupCounter[j][1]++;
+                newLetter = false;
+            }
+        }
+        if (newLetter == true) {
+            tupCounter.push([textChar, 1]);
+        }
+    }
+    tupCounter.forEach(function (tup) { return count += (tup[1] === 1 ? 0 : 1); });
+    return count;
+};
+var answer = duplicateCount2("scootersrateac");
+console.log(answer);
--- challenge1.ts
+++ challenge1.ts
@@ -4,4 +4,33 @@ const isDivisible = (n:number, x:number, y:number):boolean => n % x === 0 && n %
 
 //count the characters that are duplicated
 
-const duplicateCount = (text: string): number => text.toLowerCase().split("").sort().reduce((acc,cur,ind,arr)=> acc += (cur == arr[ind+1] && cur != arr[ind-1]) ? 1 : 0, 0 )
+const duplicateCount = (text: string): number => text.toLowerCase().split("").sort().reduce((acc,cur,ind,arr)=> acc += (cur == arr[ind+1] && cur != arr[ind-1]) ? 1 : 0, 0 );
+
+const duplicateCount2 = (text: string): number => {
+    let tupCounter : [string, number][] = [];
+    let count : number = 0;
+
+    for(let textChar of text.toLowerCase()){
+    
+        let newLetter : boolean = true;
+        for(let j = 0; j < tupCounter.length; j++){
+    
+            if(textChar === tupCounter[j][0]){
+                tupCounter[j][1]++;
+                newLetter = false;
+            }
+        }
+    
+        if(newLetter == true){
+            tupCounter.push([textChar,1]);
+        } 
+    }
+    
+    tupCounter.forEach(tup=> count += (tup[1] === 1 ? 0 : 1) );
+
+    return count;
+}
+
+const answer : number = duplicateCount2("scootersrateac");
+
+console.log(answer);
